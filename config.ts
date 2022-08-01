f94029b954cd291319a4836de3cc39c74c9a2f4e
Jiří Bažant <j.bazant@gmail.com>
2022-08-01 23:54:14 +0200

    chore: setup eslint

---
+++ config.ts
@@ -0,0 +1,1 @@
+const apiUrl = 'https://raw.githubusercontent.com/24i/smartapps-test/main';
--- jest/setup.js
+++ jest/setup.js
@@ -1,5 +1,3 @@
 import nock from 'nock';
 
 nock.disableNetConnect();
-global.nockToCloud = () =>
-  nock('https://raw.githubusercontent.com/24i/smartapps-test/main');
