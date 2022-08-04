1d3c09bbbbc004179b0376869a0cd084602013fd
Jiří Bažant <j.bazant@gmail.com>
2022-08-04 21:06:21 +0200

    fix: mock react-navigation-shared-element for tests

--- jest/setup.js
+++ jest/setup.js
@@ -3,3 +3,8 @@
 import nock from 'nock';
 
 nock.disableNetConnect();
+
+jest.mock('react-navigation-shared-element', () => ({
+  createSharedElementStackNavigator: jest.fn(),
+  SharedElement: ({ children }) => children,
+}));
